040541642e069a475fffcb5f9e4b55604f97bebe
Juan Trelles <juantrelles@gmail.com>
2022-12-08 16:08:10 -0600

    handling captions on next pages

--- pdfigcapx/src/models.py
+++ pdfigcapx/src/models.py
@@ -20,15 +20,6 @@ class TextContainer(BaseModel):
         return [self.x0, self.y0, self.width, self.height]
 
 
-class HtmlPage(BaseModel):
-    name: str
-    width: int
-    height: int
-    text_containers: Optional[list[TextContainer]] = None
-    img_name: str
-    page_number: int
-
-
 class Caption(BaseModel):
     text: str
     x0: int
@@ -45,8 +36,41 @@ class Figure(BaseModel):
     y0: int
     width: int
     height: int
-    caption: Caption
+    caption: Optional[Caption] = None
     multicolumn: bool
+    identifier: Optional[str]
+    type: Optional[str]
+    sweep_type: Optional[str]
 
     def to_bbox(self):
         return [self.x0, self.y0, self.width, self.height]
+
+
+class HtmlPage(BaseModel):
+    """Page in a PDF document converted to HTML
+    attributes:
+    - name:   File name
+    - width:  Width of HTML page which differs from the width of the PNG file
+    - height: Height of HTML page which differs from the height of the PNG file
+    - text_containers: Every div inside the HTML containing text
+    - img_name: Associated PNG filename
+    - page_number: Page number in PDF
+    - orphan_figure: Figure in page built from remaining contour candidates
+        after every other candidates have been matched to captions. This
+        situation is common when the caption is on the next page, and the field
+        stores the caption-less figure for a posterior validation across
+        consecutive pages.
+    - orphan_captions: div containing the starting word 'figure' that were
+        not matched to any candidate region on the page after sweeping the
+        page downwards, upwards and sidewards.
+    """
+
+    name: str
+    width: int
+    height: int
+    text_containers: Optional[list[TextContainer]] = None
+    img_name: str
+    page_number: int
+    figures: Optional[list[Figure]] = []
+    orphan_figure: Optional[Figure] = None
+    orphan_captions: Optional[list[TextContainer]] = []
